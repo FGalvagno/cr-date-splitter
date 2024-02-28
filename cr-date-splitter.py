77a868b9224be83e8fe970004f0e95c777ea6255
Facundo Galvagno <galvagno.facundo@outlook.com>
2024-02-28 09:55:02 -0300

    added multi cfg support

--- cr-date-splitter.py
+++ cr-date-splitter.py
@@ -1,31 +1,41 @@
-import yaml
 import os
 import time
 from datetime import datetime
+import glob
+
+import yaml
 
 from splitter import Splitter
 
-f = open("config/AWS.yaml", 'r')
-cfg = yaml.safe_load(f)
+
+
 
 def main():
     if not os.path.exists('export'):
         os.makedirs('export')
+    s = []
+    #f = open("config/AWS.yml", 'r')
+    #cfg = yaml.safe_load(f)
 
-    s1 = Splitter(
-        cfg["path"]["src"],
-        cfg["path"]["dest"], 
-        cfg["site"],
-        cfg["instrum"], 
-        cfg["file_name"])
+    cfg_files = glob.glob('./config/*.yml')
 
+    for cfg in cfg_files:
+        s.append(
+            Splitter(
+            cfg["path"]["src"],
+            cfg["path"]["dest"], 
+            cfg["site"],
+            cfg["instrum"], 
+            cfg["file_name"])
+        )
 
     while(True):
-        if(s1.has_changed()):
-            print("Processing changes: ", datetime.now())
-            s1.load_dat()
-            s1.split_date()
-        time.sleep(30)
+        for i in len(s):
+            if(s(i).has_changed()):
+                print("Processing changes: ", datetime.now())
+                s(i).load_dat()
+                s(i).split_date()    
+    time.sleep(30)
         
 
 
